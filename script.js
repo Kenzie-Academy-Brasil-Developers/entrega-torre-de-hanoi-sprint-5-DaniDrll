5caafc29ef181d7e262883f63be2ab9134544165
TheDevJoao <thejoao94@gmail.com>
2021-08-03 01:03:03 -0300

    Criação de torres para movimentar os discos

--- script.js
+++ script.js
@@ -1,51 +1,60 @@
-const contDisco1 = document.getElementById("contDisco1");
-const contDisco2 = document.getElementById("contDisco2");
-const contDisco3 = document.getElementById("contDisco3");
+// Criar as 3 torres
+// Criar os discos
+// Criar interceptadores das torres
+// Criar interceptadores de seleção de discos
+// Criar interceptadores de captura dos discos
+// Criar evento para mover os discos
+// Crirar condições para não deixar um disco maior sobrepor um menor
 
-const disco1 = document.getElementById("D1");
-const disco2 = document.getElementById("D2");
-const disco3 = document.getElementById("D3");
-const disco4 = document.getElementById("D4");
+const contDisco1 = document.getElementById('contDisco1');
+const contDisco2 = document.getElementById('contDisco2');
+const contDisco3 = document.getElementById('contDisco3');
 
-let container = document.getElementById("conteiner");
-container.addEventListener("click", getDisc);
+const disco1 = document.getElementById('D1');
+const disco2 = document.getElementById('D2');
+const disco3 = document.getElementById('D3');
+const disco4 = document.getElementById('D4');
 
-function getDisc (event) {
-    if (event.target.id === "D1") {
-        console.log("D1")
-    }
-    
-    if (event.target.id === "D2") {
-        console.log("D2")
+const towerCont1 = document.getElementById('tower-cont1');
+const towerCont2 = document.getElementById('tower-cont2');
+const towerCont3 = document.getElementById('tower-cont3');
 
-    }
-    
-    if (event.target.id === "D3") {
-        console.log("D3")
-    }
-    
-    if (event.target.id === "D4") {
-        console.log("D4")
-    }
+let container = document.getElementById('conteiner');
+container.addEventListener('click', getDisc);
+
+let first;
+let last;
+
+function getDisc(event) {
+  if (event.target.id === 'tower-cont1') {
+    console.log('tower1');
+  }
+  towerCont1.firstChild.appendChild(towerCont2);
+
+  if (event.target.id === 'tower-cont2') {
+    console.log('tower2');
+  }
+
+  if (event.target.id === 'tower-cont3') {
+    console.log('tower3');
+  }
 }
 
-let rod = document.getElementsByClassName("palito");
+let rod = document.getElementsByClassName('palito');
 for (let i = 0; i < rod.length; i++) {
-    rod[i].addEventListener("click", rodSwap);
+  rod[i].addEventListener('click', rodSwap);
 }
 
+function rodSwap(listen) {
+  if (listen.target.id === 'palito1') {
+    console.log('P1');
+  }
 
-function rodSwap (listen) {
-    if (listen.target.id === "palito1") {
-        console.log("P1")
-    }
-    
-    if (listen.target.id === "palito2") {
-        console.log("P2")
-        
-    }
+  if (listen.target.id === 'palito2') {
+    console.log('P2');
+  }
 
-    if (listen.target.id === "palito3") {
-        console.log("P3")
-    }
+  if (listen.target.id === 'palito3') {
+    console.log('P3');
+  }
 }
